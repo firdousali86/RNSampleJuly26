659182fe209722523bbb691aa277d3de62cc2bf9
Firdous Ali <firdous.ali.tp@gmail.com>
2023-08-29 20:03:10 +0500

    split navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,15 @@
 
 import React, {useEffect, useState} from 'react';
 import analytics from '@react-native-firebase/analytics';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-import {
-  HomeScreen,
-  LoginScreen,
-  ListScreen,
-  CartScreen,
-  MapScreen,
-  SignupScreen,
-  FirestoreScreen,
-} from './containers';
-import UserProfile from './UserProfile';
 import {PersistantHelper, NotificationHelper} from './helpers';
 import {EventRegister} from 'react-native-event-listeners';
-import {Provider} from 'react-redux';
-import store from './store';
-import auth from '@react-native-firebase/auth';
 
-const Stack = createNativeStackNavigator();
+import auth from '@react-native-firebase/auth';
+import Navigate from './navigate';
 
 function App(): JSX.Element {
-  const navigation = useNavigation();
-
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   const getUserName = async () => {
@@ -48,27 +33,6 @@ function App(): JSX.Element {
   }, []);
 
   useEffect(() => {
-    NotificationHelper.initializeFCM(
-      recievedMessage => {
-        if (recievedMessage.type === 'orderBooked') {
-          //call order api
-        }
-
-        if (recievedMessage.type === 'newemail') {
-          //call inbox api
-        }
-      },
-      tappedMessage => {
-        if (tappedMessage.type === 'news') {
-          //navigate to so n so views with data
-          // navigation.navigate('somescreenname');
-        }
-      },
-    );
-    NotificationHelper.checkFCMPermission();
-    NotificationHelper.getToken();
-    NotificationHelper.refreshToken();
-
     analytics().logEvent('testrun', {
       name: 'firdous ali',
     });
@@ -84,54 +48,9 @@ function App(): JSX.Element {
     };
   }, []);
 
-  const getAuthStack = () => {
-    return (
-      <Stack.Group>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-      </Stack.Group>
-    );
-  };
-
-  const getMainStack = () => {
-    return (
-      <Stack.Group>
-        <Stack.Screen
-          name="Firestore"
-          component={FirestoreScreen}
-          options={{title: 'FirestoreScreen'}}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen
-          name="ListScreen"
-          component={ListScreen}
-          options={{title: 'List'}}
-        />
-        <Stack.Screen name="MapScreen" component={MapScreen} />
-
-        <Stack.Screen
-          name="UserProfile"
-          component={UserProfile}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen
-          name="CartScreen"
-          component={CartScreen}
-          options={{title: 'Cart'}}
-        />
-      </Stack.Group>
-    );
-  };
-
   return (
     <NavigationContainer>
-      <Provider store={store}>
-        <Stack.Navigator>{getMainStack()}</Stack.Navigator>
-      </Provider>
+      <Navigate />
     </NavigationContainer>
   );
 }
---
+++ src/navigate/index.js
@@ -0,0 +1,94 @@
+import {useEffect} from 'react';
+import {} from 'react-native';
+import {Provider} from 'react-redux';
+import store from '../store';
+import {NotificationHelper} from '../helpers';
+import {
+  HomeScreen,
+  LoginScreen,
+  ListScreen,
+  CartScreen,
+  MapScreen,
+  SignupScreen,
+  FirestoreScreen,
+} from '../containers';
+import UserProfile from '../UserProfile';
+import {useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    NotificationHelper.initializeFCM(
+      recievedMessage => {
+        if (recievedMessage.type === 'orderBooked') {
+          //call order api
+        }
+
+        if (recievedMessage.type === 'newemail') {
+          //call inbox api
+        }
+      },
+      tappedMessage => {
+        navigation.navigate('Home');
+      },
+    );
+    NotificationHelper.checkFCMPermission();
+    NotificationHelper.getToken();
+    NotificationHelper.refreshToken();
+  }, []);
+
+  const getAuthStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Group>
+    );
+  };
+
+  const getMainStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen
+          name="Firestore"
+          component={FirestoreScreen}
+          options={{title: 'FirestoreScreen'}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen
+          name="ListScreen"
+          component={ListScreen}
+          options={{title: 'List'}}
+        />
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+
+        <Stack.Screen
+          name="UserProfile"
+          component={UserProfile}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen
+          name="CartScreen"
+          component={CartScreen}
+          options={{title: 'Cart'}}
+        />
+      </Stack.Group>
+    );
+  };
+
+  return (
+    <Provider store={store}>
+      <Stack.Navigator>{getMainStack()}</Stack.Navigator>
+    </Provider>
+  );
+};
+
+export default Navigation;
